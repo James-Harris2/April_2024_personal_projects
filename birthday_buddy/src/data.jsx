84f2e6ed3297f01ea3ee306441c30040bee47f62
James <jameseharrisii@gmail.com>
2024-04-30 09:09:05 -0700

    initialized project

---
+++ birthday_buddy/src/data.jsx
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'James E Harris II',
+      age: 46,
+      image:
+        "/assets/Trucker_James.jpg",
+    },
+    {
+      id: 2,
+      name: 'Foxy Lady_oh lala',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Rick Flare',
+      age: 54,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Snaggle Puss',
+      age: 97,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Mama Bear & 3 club_kids',
+      age: 28,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
